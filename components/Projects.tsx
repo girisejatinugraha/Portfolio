bcb588fc16db5e8f0cb48f35e2bc04414b6f0ced
Giri <136765265+girisejatinugraha@users.noreply.github.com>
2024-08-03 14:17:32 +0700

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -30,9 +30,9 @@ const Projects = () => {
               <h6 className="text-0xl">Nov 2022 - Nov 2022</h6>
             </div>
             <p className="bg-[#112240] text-sm md:text-base p-2 md:p-6 rounded-md">
-              • The landing page web design I created is simple to meet the requirements of the Mid-Semester Exam Task in the first semester.
+            • The landing page web design I created is simple to meet the requirements of the Mid-Semester Exam Task in the first semester.
               <br/>
-              • In its creation, I prioritize easy navigation and a clean appearance so that it can be easily accessed by users.
+            • In its creation, I prioritize easy navigation and a clean appearance so that it can be easily accessed by users.
             </p>
             <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
               <li>HTML</li>
@@ -62,7 +62,7 @@ const Projects = () => {
               <h6 className="text-0xl">Jan 2023 - Jan 2023</h6>
             </div>
             <p className="text-sm md:text-base bg-[#112240] p-2 md:p-6 rounded-md xl:-mr-16">
-              • This Student Attendance Application is created to fulfill the Final Semester Exam Assignment in the first semester. 
+            • This Student Attendance Application is created to fulfill the Final Semester Exam Assignment in the first semester. 
               <br/>• My main goal in creating this application is to provide an easy-to-use tool for both lecturers and students to efficiently record attendance. 
               <br/>• The main features of this application are designed with simplicity and practicality as the top priorities.
             </p>
@@ -96,7 +96,7 @@ const Projects = () => {
               <h6 className="text-0xl">Mei 2023 - Mei 2023</h6>
             </div>
             <p className="text-sm md:text-base bg-[#112240] p-2 md:p-6 rounded-md">
-              • The brand/type of this television is created to fulfill the Mid-Semester Exam Task in the second semester. 
+            • The brand/type of this television is created to fulfill the Mid-Semester Exam Task in the second semester. 
               <br/>• This project is meticulously designed to ensure that it aligns with the given task guidelines and demonstrates a strong understanding of the materials taught during that time.
             </p>
             <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
@@ -129,7 +129,7 @@ const Projects = () => {
               <h6 className="text-0xl">Jul 2023 - Jul 2023</h6>
             </div>
             <p className="text-sm md:text-base bg-[#112240] p-2 md:p-6 rounded-md xl:-mr-16">
-              • The library was created to fulfill the Final Semester Exam Task in the second semester. 
+            • The library was created to fulfill the Final Semester Exam Task in the second semester.  
               <br/>• Carefully designed, this project is aimed to meet the given task guidelines while demonstrating a profound understanding of the materials taught throughout this semester.
             </p>
             <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
@@ -158,20 +158,21 @@ const Projects = () => {
               <p className="font-titleFont text-yellow-500 text-sm tracking-wide">
                 Project 5 
               </p>
-              <h3 className="text-2xl font-bold">Hotel</h3>
-              <h6 className="text-0xl">Jul 2023 - Aug 2023</h6>
+              <h3 className="text-2xl font-bold">Hotel Booking Application</h3>
+              <h6 className="text-0xl">Jan 2024 - Jan 2024</h6>
             </div>
             <p className="text-sm md:text-base bg-[#112240] p-2 md:p-6 rounded-md">
-              • This hotel project is created to fulfill the given task guidelines, showcasing an in-depth understanding of the materials taught during this period.
+            • The Hotel Booking Application is created during leisure time to facilitate users in booking accommodations online. 
+            <br/> • Its aim is to enable users to book hotel rooms without having to leave their homes or offices, providing a more efficient and comfortable experience in trip planning.
             </p>
             <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
               <li>HTML</li>
               <li>CSS</li>
               <li>JavaScript</li>
-              <li>Laravel</li>
+              <li>PHP</li>
             </ul>
             <div className="text-2xl flex gap-4">
-              <a className="hover:text-yellow-500 duration-300" href="https://github.com/girisejatinugraha/Hotel" target="_blank">
+              <a className="hover:text-yellow-500 duration-300" href="https://github.com/girisejatinugraha/Aplikasi-Pemesanan-Hotel" target="_blank">
                 <TbBrandGithub />
               </a>
             </div>
@@ -187,24 +188,28 @@ const Projects = () => {
           </a>
           <div className="w-full xl:w-1/2 flex flex-col gap-6 justify-between items-end text-right z-10">
             <div>
-              <p className="font-titleFont text-yellow-500 text-sm tracking-wide">
+            <p className="font-titleFont text-yellow-500 text-sm tracking-wide">
                 Project 6
               </p>
-              <h3 className="text-2xl font-bold">E-Commerce Website</h3>
-              <h6 className="text-0xl">Aug 2023 - Aug 2023</h6>
+              <h3 className="text-2xl font-bold">Dashboard-E-Commerce</h3>
+              <h6 className="text-0xl">Apr 2024 - Apr 2024</h6>
             </div>
             <p className="text-sm md:text-base bg-[#112240] p-2 md:p-6 rounded-md xl:-mr-16">
-              • The E-Commerce website is created to fulfill the Final Semester Exam Task in the third semester. 
-              <br/>• This project is developed with the goal of demonstrating an understanding of web development concepts and practical applications.
+            • Know the number of transactions in our e-commerce, the total income earned, and the total products sold in our e-commerce per month.
+            <br/>• Find out the number of transactions and total income based on store type.
+            <br/>• Know how our customers are distributed based on city and gender.
             </p>
             <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>JavaScript</li>
-              <li>Laravel</li>
+              <li>Data Visualization</li>
+              <li>Microsoft Excel</li>
+              <li>Tableau</li>
             </ul>
             <div className="text-2xl flex gap-4">
-              <a className="hover:text-yellow-500 duration-300" href="https://github.com/girisejatinugraha/E-Commerce" target="_blank">
+              <a
+                className="hover:text-yellow-500 duration-300"
+                href="https://github.com/girisejatinugraha/Dashboard-E-Commerce"
+                target="_blank"
+              >
                 <TbBrandGithub />
               </a>
             </div>
@@ -223,20 +228,20 @@ const Projects = () => {
               <p className="font-titleFont text-yellow-500 text-sm tracking-wide">
                 Project 7 
               </p>
-              <h3 className="text-2xl font-bold">School</h3>
-              <h6 className="text-0xl">Aug 2023 - Aug 2023</h6>
+              <h3 className="text-2xl font-bold">School Website</h3>
+              <h6 className="text-0xl">Jul 2024 - Jul 2024</h6>
             </div>
             <p className="text-sm md:text-base bg-[#112240] p-2 md:p-6 rounded-md">
-              • The school project is created to meet the requirements of a specific task, focusing on applying and demonstrating practical knowledge in web development.
+            • The School Website was created to facilitate communication and access to information between the school, students, and parents online.
+            <br/> • This website provides various features such as school information, educational programs, activity galleries, and contact information to facilitate interaction among all parties involved.
             </p>
             <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>JavaScript</li>
-              <li>Laravel</li>
+              <li>React.js</li>
+              <li>GatsbyJS</li>
+              <li>SCSS</li>
             </ul>
             <div className="text-2xl flex gap-4">
-              <a className="hover:text-yellow-500 duration-300" href="https://github.com/girisejatinugraha/Sekolah" target="_blank">
+              <a className="hover:text-yellow-500 duration-300" href="https://github.com/girisejatinugraha/Website-Sekolah" target="_blank">
                 <TbBrandGithub />
               </a>
             </div>
