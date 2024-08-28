93a724217033b99c5ef827ec435a576ca35cdcc7
Giri <136765265+girisejatinugraha@users.noreply.github.com>
2024-08-28 20:20:11 +0700

    project selesai

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -4,29 +4,27 @@ import { AiFillThunderbolt } from "react-icons/ai";
 import { useInView } from "react-intersection-observer";
 
 const skillsData = {
-  hardSkills: {
-    "Data Analyst & Scientist": [
-      "Microsoft Excel",
-      "Tableau",
-      "SQL",
-      "Python",
-      "R",
-      "Data Visualization",
-    ],
-    "Web Developer": [
-      "HTML",
-      "CSS & SCSS",
-      "Tailwind CSS",
-      "JavaScript",
-      "PHP",
-      "Laravel",
-      "React.Js",
-      "GatsbyJS",
-      "Git",
-      "GitHub",
-    ],
-  },
-  softSkills: [
+  "Data Analyst & Scientist": [
+    "Microsoft Excel",
+    "Tableau",
+    "SQL",
+    "Python",
+    "R",
+    "Data Visualization",
+  ],
+  "Web Developer": [
+    "HTML",
+    "CSS & SCSS",
+    "Tailwind CSS",
+    "JavaScript",
+    "PHP",
+    "Laravel",
+    "React.Js",
+    "GatsbyJS",
+    "Git",
+    "GitHub",
+  ],
+  SoftSkills: [
     "Communication",
     "Critical Thinking",
     "Problem Solving",
@@ -36,7 +34,8 @@ const skillsData = {
 };
 
 const Skills = () => {
-  const { ref: refHardSkills, inView: inViewHardSkills } = useInView({ triggerOnce: true });
+  const { ref: refDataAnalyst, inView: inViewDataAnalyst } = useInView({ triggerOnce: true });
+  const { ref: refWebDeveloper, inView: inViewWebDeveloper } = useInView({ triggerOnce: true });
   const { ref: refSoftSkills, inView: inViewSoftSkills } = useInView({ triggerOnce: true });
 
   return (
@@ -45,55 +44,71 @@ const Skills = () => {
       className="max-w-containerSmall mx-auto py-10 lgl:py-32 flex flex-col gap-8"
     >
       <SectionTitle title="Personal Skills" titleNo="" />
-      <div ref={refHardSkills} className="flex flex-col lgl:flex-row gap-16">
-        {Object.keys(skillsData.hardSkills).map((category, categoryIndex) => {
-          // Check if the category exists in skillsData.hardSkills
-          const skills = skillsData.hardSkills[category];
-          
-          // Ensure skills is an array
-          if (!Array.isArray(skills)) {
-            console.error(`Expected an array but received: ${skills}`);
-            return null;
-          }
-          return (
-            <motion.div
-              key={categoryIndex}
-              initial={{ opacity: 0, y: 20 }}
-              animate={inViewHardSkills ? { opacity: 1, y: 0 } : {}}
-              exit={{ opacity: 0, y: 20 }}
-              transition={{ duration: 0.5, delay: 0.2 }}
-              className="w-full lgl:w-1/2"
-            >
-              <h3 className="text-xl font-semibold mb-4">{category}</h3>
-              <ul className="grid grid-cols-2 gap-4">
-                {skills.map((skill, index) => (
-                  <motion.li
-                    key={index}
-                    initial={{ opacity: 0, y: 20 }}
-                    animate={inViewHardSkills ? { opacity: 1, y: 0 } : {}}
-                    exit={{ opacity: 0, y: 20 }}
-                    transition={{ duration: 0.5, delay: index * 0.2 }}
-                    className="flex items-center gap-2 text-sm font-medium"
-                  >
-                    <AiFillThunderbolt className="text-yellow-500" />
-                    {skill}
-                  </motion.li>
-                ))}
-              </ul>
-            </motion.div>
-          );
-        })}
+
+      <div className="flex flex-col lgl:flex-row gap-16">
+        <motion.div
+          ref={refDataAnalyst}
+          initial={{ opacity: 0, y: 20 }}
+          animate={inViewDataAnalyst ? { opacity: 1, y: 0 } : {}}
+          exit={{ opacity: 0, y: 20 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className="w-full lgl:w-1/3"
+        >
+          <h3 className="text-xl font-semibold mb-4">Data Analyst & Scientist</h3>
+          <ul className="grid grid-cols-2 gap-4">
+            {skillsData["Data Analyst & Scientist"].map((skill, index) => (
+              <motion.li
+                key={index}
+                initial={{ opacity: 0, y: 20 }}
+                animate={inViewDataAnalyst ? { opacity: 1, y: 0 } : {}}
+                exit={{ opacity: 0, y: 20 }}
+                transition={{ duration: 0.5, delay: index * 0.2 }}
+                className="flex items-center gap-2 text-sm font-medium"
+              >
+                <AiFillThunderbolt className="text-yellow-500" />
+                {skill}
+              </motion.li>
+            ))}
+          </ul>
+        </motion.div>
+
+        <motion.div
+          ref={refWebDeveloper}
+          initial={{ opacity: 0, y: 20 }}
+          animate={inViewWebDeveloper ? { opacity: 1, y: 0 } : {}}
+          exit={{ opacity: 0, y: 20 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className="w-full lgl:w-1/3"
+        >
+          <h3 className="text-xl font-semibold mb-4">Web Developer</h3>
+          <ul className="grid grid-cols-2 gap-4">
+            {skillsData["Web Developer"].map((skill, index) => (
+              <motion.li
+                key={index}
+                initial={{ opacity: 0, y: 20 }}
+                animate={inViewWebDeveloper ? { opacity: 1, y: 0 } : {}}
+                exit={{ opacity: 0, y: 20 }}
+                transition={{ duration: 0.5, delay: index * 0.2 }}
+                className="flex items-center gap-2 text-sm font-medium"
+              >
+                <AiFillThunderbolt className="text-yellow-500" />
+                {skill}
+              </motion.li>
+            ))}
+          </ul>
+        </motion.div>
+
         <motion.div
           ref={refSoftSkills}
           initial={{ opacity: 0, y: 20 }}
           animate={inViewSoftSkills ? { opacity: 1, y: 0 } : {}}
           exit={{ opacity: 0, y: 20 }}
           transition={{ duration: 0.5, delay: 0.2 }}
-          className="w-full lgl:w-1/2 mt-8 lgl:mt-0"
+          className="w-full lgl:w-1/3 mt-8 lgl:mt-0"
         >
           <h3 className="text-xl font-semibold mb-4">Soft Skills</h3>
           <ul className="grid grid-cols-2 gap-4">
-            {skillsData.softSkills.map((skill, index) => (
+            {skillsData.SoftSkills.map((skill, index) => (
               <motion.li
                 key={index}
                 initial={{ opacity: 0, y: 20 }}
